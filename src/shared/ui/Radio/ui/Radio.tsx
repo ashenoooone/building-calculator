7520cbe341b3c9eb3152bec7818031bc001e2c7d
Roman Gontar <ashenoooone@gmail.com>
2023-11-02 18:57:49 +0400

    feat radio box

--- src/shared/ui/Radio/ui/Radio.tsx
+++ src/shared/ui/Radio/ui/Radio.tsx
@@ -26,6 +26,7 @@ export const Radio = memo((props: RadioProps) => {
 				{...rest}
 			/>
 			<svg
+				className={cls.svg}
 				width='20px'
 				height='20px'
 				viewBox='0 0 20 20'
@@ -37,11 +38,11 @@ export const Radio = memo((props: RadioProps) => {
 				/>
 				<path
 					d='M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z'
-					className='inner'
+					className={cls.inner}
 				/>
 				<path
 					d='M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z'
-					className='outer'
+					className={cls.outer}
 				/>
 			</svg>
 			<span>{text}</span>
